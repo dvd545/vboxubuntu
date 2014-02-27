b067be9b1d680b2ed3c663551faba3861557cc15
dvd545 <schmdt628@gmail.com>
2014-02-27 19:15:56 +0000

    updated files

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -1,1 +1,12 @@
 var expect = require
+describe("Tags", function(){
+   describe("#parse()", function(){
+       it("should parse long formed tags", function(){
+           var args = ["--depth=4", "--hello=world"];
+           var results = tags.parse(args);
+ 
+           expect(results).to.have.a.property("depth", 4);
+           expect(results).to.have.a.property("hello", "world");
+       });
+   });
+});
