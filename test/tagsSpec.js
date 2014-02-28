4aa8a8ac53040b6ae7e632f7ade9fb20c0c9ed91
dvd545 <schmdt628@gmail.com>
2014-02-28 23:05:21 +0000

    added second test

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -1,12 +1,26 @@
-var expect = require
-describe("Tags", function(){
-   describe("#parse()", function(){
-       it("should parse long formed tags", function(){
-           var args = ["--depth=4", "--hello=world"];
-           var results = tags.parse(args);
+var expect = require("chai").expect;
+var tags = require("../lib/tags.js");
  
-           expect(results).to.have.a.property("depth", 4);
-           expect(results).to.have.a.property("hello", "world");
-       });
+describe("#parse()", function(){
+       it("should parse long formed tags and convert numbers", function(){
+			var args = ["--depth=4", "--hello=world"];
+			var results = tags.parse(args);
+		 
+			expect(results).to.have.a.property("depth", 4);
+			expect(results).to.have.a.property("hello", "world");
+		});
+		it("should fallback to defaults", function(){
+			var args = ["--depth=4", "--hello=world"];
+			var defaults = { depth: 2, foo: "bar" };
+			var results = tags.parse(args, defaults);
+		 
+			var expected = {
+				depth: 4,
+				foo: "bar",
+				hello: "world"
+			};
+		 
+			expect(results).to.deep.equal(expected);
+		});
+	   
    });
-});
