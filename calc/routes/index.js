6143f958ae5f8eefca0bcbaba7b0239d01ad8000
dvd545 <schmdt628@gmail.com>
2014-03-28 20:40:32 +0000

    updated to work with html

--- calc/routes/index.js
+++ calc/routes/index.js
@@ -1,39 +1,30 @@
-
-/*
- * GET home page.
- */
-var math = require("../lib/calc.js");
-
+var math = require("../lib/cfunction.js");
 
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Calculator Project' });
-
+  res.render('index', { title: 'Calculator' });
 };
 
-exports.value = function(req,res){
-  var num1;
-  var num2;
-  var op;
-  var solution;
-  num1 = number(req.body.num1);
-  num2 = numner(req.body.num2);
-  op = req.body.op;
-  if (op == '+'){
-	solution = math.add(num, num2);
-  } else if (op == '-'){
-    solution = math.subtract(num1, num2);
-  } else if  (op == '*'){
-	solution = math.mulitply(num1, num2);
-  } else {
-    solution = math.divide(num1,num2);
-  }
-  
-  res.render('index', {title: 'Calculator Project', answer: solution });
-  solution = null;
+exports.value = function(req, res){
+	var calc = String(req.query.calculation);
+	var num1 = Number(req.query.x);
+	var num2 = Number(req.query.y);
+	var ans;
+	console.log("Calculator Function:" + calc);
+	if (calc == '+') {
+		ans = math.add(num1,num2);
+	} else if (calc == '-') {
+		ans = math.subtract(num1,num2);
+	} else if (calc == '*') {
+		ans = math.multiply(num1,num2);
+	} else {
+		ans = math.divide(num1,num2);
+	}
 
+	res.render('index', { title: 'Calculator', answer: ans });
+  ans = null;
 };
 
-exports.test = function(req, res){
-  res.render('test', { title : 'Calculator Project'});
+exports.test = function(req,res){
+  res.render('test', { title: 'Calculator'});
 };
