66bf55194957a00ce56e8b35db069f37c027809d
dvd545 <schmdt628@gmail.com>
2014-03-28 20:12:33 +0000

    fixed routing

--- calc/app.js
+++ calc/app.js
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.post('/value', routes.value);
+app.get('/value', routes.value);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
